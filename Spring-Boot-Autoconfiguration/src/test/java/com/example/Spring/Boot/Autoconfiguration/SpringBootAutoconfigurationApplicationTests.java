9103c81bdd192c52a52ab2de28331a5a30b4068d
Rushikesh Chavan <rn@gmail.com>
2024-07-17 23:12:32 +0530

    Autoconfiguration and Maven

---
+++ Spring-Boot-Autoconfiguration/src/test/java/com/example/Spring/Boot/Autoconfiguration/SpringBootAutoconfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring.Boot.Autoconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAutoconfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
