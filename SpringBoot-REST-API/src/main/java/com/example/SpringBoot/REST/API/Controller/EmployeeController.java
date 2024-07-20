3551e0d64f007eb876524d811d70d6cb3ea8d747
Rushikesh Chavan <rn@gmail.com>
2024-07-20 12:26:09 +0530

    Practice Postman

--- SpringBoot-REST-API/src/main/java/com/example/SpringBoot/REST/API/Controller/EmployeeController.java
+++ SpringBoot-REST-API/src/main/java/com/example/SpringBoot/REST/API/Controller/EmployeeController.java
@@ -6,24 +6,42 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
+
+
 @RestController
-@RequestMapping(path="/employee")
+
 public class EmployeeController {
 
-//    @GetMapping(path ="/getMessage")
-//    public String getMessage(){
-//        return "Hi from Controller";
-//    }
+    @GetMapping(path ="/getMessage")
+    public String getMessage(){
+        return "Hi from Controller";
+    }
 
     @GetMapping(path="/employees/{employeeId}")
     public EmployeeDTO  getEmployeeById(@PathVariable Long employeeId){
             return new EmployeeDTO(employeeId, "Rushi","rushi@gmail.com",23);
 
-    @GetMapping(path="/employee")
-            public String getAllEmployee(){
-                return "HI";
-        }
+    }
+
+    @GetMapping(path ="/getEmployeeById")
+    public String getEmployeeById(@RequestParam (required = false) Integer age , @RequestParam (required = false) String name){
+        return "Hi from Controller" +age +"  "+ name;
+    }
+
+    @PostMapping(path ="/PostMapping")
+    public String DisplayString(){
+        return "hi from PostMapping";
+    }
 
 
+    @PostMapping(path ="/PostMappingJson")
+    public EmployeeDTO CreateEmp(@RequestBody EmployeeDTO InputEmployee){
+        InputEmployee.setId(100L);
+        return InputEmployee;
+    }
+
+    @PutMapping(path="/PutMapping")
+    public String getPutMapping(){
+        return "hello from putMapping";
     }
 }
